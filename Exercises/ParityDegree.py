8e13ca592ce95551d2d6d7fb66ddf816a3c770fb
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 14:30:01 +0200

    Update ParityDegree.py

--- Exercises/ParityDegree.py
+++ Exercises/ParityDegree.py
@@ -1,3 +1,8 @@
+# A positive integer N is given. The goal is to find the highest power of 2 that divides N. 
+# In other words, we have to find the maximum K for which N modulo 2^K is 0.
+
+# Function that, given a positive integer N, returns the highest power of 2 that divides N.
+
 def solution(N):
     i = 0
     while N % 2 == 0:
