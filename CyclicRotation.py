0c8b4d001d97f3c96136b17bf78fc94242fe760b
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 13:58:50 +0200

    Update CyclicRotation.py

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -1,3 +1,8 @@
+# An array A consisting of N integers is given. Rotation of the array means that each element
+# is shifted right by one index, and the last element of the array is moved to the first place.
+
+# Function that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+
 A = [1,2,3,4,5,6]
 K = 3
 
