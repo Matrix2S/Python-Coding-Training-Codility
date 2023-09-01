faa73240da09bb8e641283b8b2d6573ca6676c40
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 14:11:08 +0200

    Update FrogJump.py

--- FrogJump.py
+++ FrogJump.py
@@ -1,3 +1,8 @@
+# A small frog wants to get to the other side of the road. The frog is currently located at position X
+# and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
+
+# Function that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+
 def solution(X,Y,Z):
     if (Y - X)%D == 0:
         return (Y - X)//D
