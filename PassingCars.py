2d086f1032dc43b7f80f8b8c3a83497daf48489d
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 14:20:14 +0200

    Update PassingCars.py

--- PassingCars.py
+++ PassingCars.py
@@ -1,3 +1,12 @@
+# A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+# Array A contains only 0s and/or 1s:
+
+        # - 0 represents a car traveling east,
+        # - 1 represents a car traveling west.
+
+
+# Function that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
+
 A = [1, 1, 1, 1, 1]
 
 def solution(A):
