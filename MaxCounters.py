3c5f157e2a9ec0f9769abb08596ceb3e96138d98
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 14:18:44 +0200

    Update MaxCounters.py

--- MaxCounters.py
+++ MaxCounters.py
@@ -1,3 +1,16 @@
+# You are given N counters, initially set to 0, and you have two possible operations on them:
+
+        # - increase(X) − counter X is increased by 1,
+        # - max counter − all counters are set to the maximum value of any counter.
+
+# A non-empty array A of M integers is given. This array represents consecutive operations:
+
+        # - if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+        # - if A[K] = N + 1 then operation K is max counter.
+
+
+# Function that, given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
+
 def solution(N, A):
     counter = [0]*N
     for i in range(len(A)):
