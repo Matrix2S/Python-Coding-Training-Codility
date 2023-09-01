8a67b475e3593af320eed9a2e266a5ed03c1b2f3
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 14:13:52 +0200

    Update TapeEquilibrium.py

--- TapeEquilibrium.py
+++ TapeEquilibrium.py
@@ -1,3 +1,9 @@
+# A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+# Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+# The difference between the two parts is the absolute difference between the sum of the first part and the sum of the second part.
+
+# Function that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+
 def solution(A):
     p = []
     for P in range(len(A)-1):
