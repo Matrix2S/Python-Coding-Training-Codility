ae3e2ee524447d051b341dcdd1f9f018b01da9a3
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 14:11:59 +0200

    Update PermMissingElem.py

--- PermMissingElem.py
+++ PermMissingElem.py
@@ -1,3 +1,8 @@
+# An array A consisting of N different integers is given. 
+# The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+# Function that, given an array A, returns the value of the missing element.
+
 def solution(A):
     i = 1
     while i in A:
