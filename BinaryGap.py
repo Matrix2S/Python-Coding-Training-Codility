a839abeeed7a54cf5ac705995fe8a61681dc303d
Matrix2S <141502620+Matrix2S@users.noreply.github.com>
2023-09-01 13:54:39 +0200

    Update BinaryGap.py

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,5 +1,8 @@
 # A binary gap within a positive integer N is any maximal sequence of consecutive zeros
 # that is surrounded by ones at both ends in the binary representation of N
+
+# Function given a positive integer N, returns the length of its longest binary gap.
+# The function should return 0 if N doesn't contain a binary gap.
 import re
 
 
